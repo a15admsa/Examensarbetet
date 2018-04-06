fdda1e400e264938cbdd1c7a9e35a8f4a09dce38
a15admsa <privat@admirsahman.se>
2018-04-06 13:10:27 +0200

    Changed the wrong file in the last commit and made changes to the welcome-file used by Laravel

--- Laravel/resources/views/master.blade.php
+++ Laravel/resources/views/master.blade.php
@@ -1,1 +1,19 @@
-@extends('master')
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel</title>
+    <link href="{{ asset('style.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <a href="/laravel/public">
+      <img src="{{asset('logo.png')}}" alt="logo" id="logo">
+    </a>
+    <form class="form" method="post">
+      <input type="text" id="searchBox" name="searchBox" size="40" maxlength="128">
+      <input type="submit" name="search" value="Search">
+    </form>
+    <h1>@yield('content')</h1>
+
+  </body>
+</html>

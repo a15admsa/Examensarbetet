866c180b6fd59f930db6256cfc169bca206310d9
a15admsa <privat@admirsahman.se>
2018-04-09 18:24:27 +0200

    Added a Model for Posts and changed the routes so it adds and selects posts from the database

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -11,15 +11,40 @@
 |
 */
 
+use Illuminate\Support\Facades\DB;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+Route::get('/add', function (){
+    DB::insert('insert into posts(title, body) values (?,?)', ['2 with Laravel', 'PHP IS THE BEST THING EVER AND LARAVEL MAKES IT AWESOME']);
+});
+
+
+
+Route::get('/read/{id}', function ($id){
+   $results = DB::select('select * from posts where id = ?', [$id]);
+   if (!empty($results)){
+       foreach ($results as $post){
+           return $post->title;
+       }
+   } else return "No results found";
+});
+
+
+
 Route::get('/post', 'PostController@index');
+
+
 
 Route::get('/search', function () {
     return view('search');
 });
+
+
 
 Route::group(['middleware' => ['web']],  function(){
 
